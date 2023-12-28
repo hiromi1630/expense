5c8daed6222b5fdc1ff3a53fe1ac0fc0c384ea00
hiromi1630 <bigsea1630@docomo.ne.jp>
2023-12-28 22:54:05 +0900

    初めてのコミット

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class InputForm(Widget):
+	pass
+
+class Expense(Widget):
+	pass
+
+class MainApp(App):
+	def build(self):
+		return Expense()
+
+if __name__ == "__main__":
+	MainApp().run()

195009ff887843ad9ccdd901e76f3b04c9f7d924
hiromi1630 <bigsea1630@docomo.ne.jp>
2023-12-29 18:52:24 +0900

    内容種別を入力可能にした

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
+item_list = ["食費", "交通費", "書籍代", "娯楽費", "収入", "その他"]
+
 class InputForm(Widget):
 	# 家計簿登録処理
 	def expense_register(self):
 		# 日付を読み取る
 		date = self.ids.input_date.text
+		# 種別を読み取る
+		item = self.ids.input_item_code.text
+		item_code = item_list.index(item) + 1
 		# 内訳を読み取る
-		item_code = self.ids.input_content.text
+		content = self.ids.input_content.text
 		# 金額を読み取る
 		money = self.ids.input_money.text
 		try:
 			c.execute(f"""
-			INSERT INTO acc_data(acc_date, item_code, amount)
-			VALUES('{date}',{item_code},{money})
+			INSERT INTO acc_data(acc_date, item_code, content, amount)
+			VALUES('{date}',{item_code},'{content}',{money})
 			""")
 			c.execute("COMMIT;")
+			# 入力フォーム内のテキストを削除(重複登録防止)
 			self.ids.input_date.text = ""
 			self.ids.input_content.text = ""
 			self.ids.input_money.text = ""
@@ -42,34 +48,38 @@ c.execute("PRAGMA foreign_keys = 1")
 
 # 既にデータベースが存在する場合エラーになるので回避
 try:    
-    # itemテーブルの定義
-    ddl = """
-    CREATE TABLE item
-    (
-        item_code INTEGER PRIMARY KEY,
-        item_name TEXT NOT NULL UNIQUE
-    );
-    """
-    # SQLの発行
-    c.execute(ddl)
+	# itemテーブルの定義
+	ddl = """
+	CREATE TABLE item
+	(
+		item_code INTEGER PRIMARY KEY,
+		item_name TEXT NOT NULL UNIQUE
+	);
+	"""
+	# SQLの発行
+	c.execute(ddl)
 
-    # acc_dataテーブルの定義
-    ddl = """
-    CREATE TABLE acc_data
-    ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        acc_date DATE NOT NULL,
-        item_code INTEGER NOT NULL,
-        amount INTEGER,
-        FOREIGN KEY(item_code) REFERENCES item(item_code)
-    );
-    """
-    # itemテーブルへリファレンスデータの登録
-    c.execute(ddl)
-    c.execute("INSERT INTO item(item_name) VALUES('食費');")
-    c.execute("INSERT INTO item(item_name) VALUES('交通費');")
-    c.execute("INSERT INTO item(item_name) VALUES('書籍代');")
-    c.execute("COMMIT;")
+	# acc_dataテーブルの定義
+	ddl = """
+	CREATE TABLE acc_data
+	( 
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		acc_date DATE NOT NULL,
+		item_code INTEGER NOT NULL,
+		content TEXT NOT NULL,
+		amount INTEGER NOT NULL,
+		FOREIGN KEY(item_code) REFERENCES item(item_code)
+	);
+	"""
+	# itemテーブルへリファレンスデータの登録
+	c.execute(ddl)
+	c.execute("INSERT INTO item(item_name) VALUES('食費');")
+	c.execute("INSERT INTO item(item_name) VALUES('交通費');")
+	c.execute("INSERT INTO item(item_name) VALUES('書籍代');")
+	c.execute("INSERT INTO item(item_name) VALUES('娯楽費');")
+	c.execute("INSERT INTO item(item_name) VALUES('収入');")
+	c.execute("INSERT INTO item(item_name) VALUES('その他');")
+	c.execute("COMMIT;")
 except:
     pass
 
